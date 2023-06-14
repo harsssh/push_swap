c33f2c9fb74c2288823ea83d4b2323fe8ca03bcc
harsssh <66548698+harsssh@users.noreply.github.com>
2023-06-15 07:05:50 +0900

    replace

--- src/algorithm/optimize.c
+++ src/algorithm/optimize.c
@@ -6,7 +6,7 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 05:54:35 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/06/15 06:15:20 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/06/15 06:47:38 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include "util/util.h"
 #include <stdlib.h>
 
-static bool	is_equal(const char *s, const char *t)
+static bool	streq(const char *s, const char *t)
 {
 	return (ft_strcmp(s, t) == 0);
 }
@@ -31,14 +31,45 @@ static void	remove_adjacent(char **array, const char *s, const char *t)
 	j = 0;
 	while (array[i])
 	{
-		if ((is_equal(array[i], s) && j > 0 && is_equal(array[j - 1], t))
-			|| (is_equal(array[i], t) && j > 0 && is_equal(array[j - 1], s)))
+		if (j > 0 && ((streq(array[i], s) && streq(array[j - 1], t))
+				|| (streq(array[i], t) && streq(array[j - 1], s))))
 		{
 			free(array[i]);
 			free(array[j - 1]);
 			array[i] = NULL;
 			array[j - 1] = NULL;
 			--j;
+		}
+		else
+		{
+			array[j] = array[i];
+			if (i != j)
+				array[i] = NULL;
+			++j;
+		}
+		++i;
+	}
+}
+
+static void	replace_sequence(char **array, const char *s, const char *t,
+		const char *u)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (array[i])
+	{
+		if (j > 1 && streq(array[j - 2], s) && streq(array[j - 1], t)
+			&& streq(array[i], u))
+		{
+			free(array[i]);
+			free(array[j - 2]);
+			array[i] = NULL;
+			array[j - 2] = ft_strdup(INST_SA);
+			if (array[j - 2] == NULL)
+				exit_with_message();
 		}
 		else
 		{
@@ -74,6 +105,7 @@ void	optimize_instructions(t_stacks *stacks)
 	free(stacks->instructions);
 	remove_adjacent(array, INST_PA, INST_PB);
 	remove_adjacent(array, INST_RA, INST_RRA);
+	replace_sequence(array, INST_RA, INST_PB, INST_RRA);
 	stacks->instructions = join_all(array, "\n");
 	free_array(array);
 }
