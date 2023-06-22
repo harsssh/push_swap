4a5f516d6011faeef7f2bfb25bff39e465217bdd
harsssh <66548698+harsssh@users.noreply.github.com>
2023-06-22 18:33:07 +0900

    update stacks init

--- src/stacks/stacks.c
+++ src/stacks/stacks.c
@@ -6,12 +6,13 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 02:00:23 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/06/22 17:49:51 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/06/22 18:30:46 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "deque/deque.h"
 #include "error/error.h"
+#include "ft_string.h"
 #include "stacks.h"
 #include <stdlib.h>
 
@@ -21,7 +22,7 @@ void	stacks_init(t_stacks *stacks, size_t capacity)
 	stacks->b = deque_init(capacity);
 	if (stacks->a == NULL || stacks->b == NULL)
 		exit_with_message();
-	stacks->instructions = NULL;
+	stacks->instructions = ft_strdup("");
 	stacks->partitions = 1;
 }
 
