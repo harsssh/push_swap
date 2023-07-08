63347bc0c82562f83bbc597a4abdfd670a462f7a
harsh <66548698+harsssh@users.noreply.github.com>
2023-07-08 20:29:55 +0900

    WIP: improve worst case

--- src/algorithm/sort_stacks.c
+++ src/algorithm/sort_stacks.c
@@ -6,7 +6,7 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 12:08:28 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/07/08 13:37:22 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/07/08 19:09:05 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "deque/deque.h"
 #include "helper.h"
 #include "optimize.h"
+#include "stacks/stacks.h"
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -100,6 +101,70 @@ static bool	should_terminate(t_stacks *stacks, size_t n)
 	return (false);
 }
 
+#include "error/error.h"
+#include <stdlib.h>
+void get_min_max(t_deque *deque, int *min, int *max)
+{
+	size_t	i;
+	int		*array;
+	size_t n = deque_size(deque);
+
+	array = malloc(sizeof(int) * n);
+	if (!array)
+		exit_with_message();
+	i = 0;
+	while (i < n)
+	{
+		array[i] = deque_at_back(deque, i);
+		i++;
+	}
+	*min = quick_select(array, n, 0);
+	*max = quick_select(array, n, n - 1);
+	free(array);
+}
+
+static void f(t_stacks *stacks)
+{
+	int min, max;
+	size_t count = 0;
+	while (!stacks->b->is_empty)
+	{
+		get_min_max(stacks->b, &min, &max);
+		size_t n = deque_size(stacks->b);
+		size_t i = 0;
+		if (n == 1)
+		{
+			stacks_pa(stacks);
+			break;
+		}
+		while (deque_at_back(stacks->b, i) != max)
+			i++;
+		void (*rotate)(t_stacks *);
+		if (i < n / 2)
+			rotate = stacks_rb;
+		else
+			rotate = stacks_rrb;
+		while (true)
+		{
+			int top = deque_at_back(stacks->b, 0);
+			if (top == min)
+			{
+				stacks_pa(stacks);
+				stacks_ra(stacks);
+				++count;
+			} else if (top == max)
+			{
+				stacks_pa(stacks);
+				break;
+			} else {
+				rotate(stacks);
+			}
+		}
+	}
+	while (count--)
+		stacks_rra(stacks);
+}
+
 // sort the first n elements of stack a in ascending order
 static void	do_sort_stacks(t_stacks *stacks, size_t n)
 {
@@ -112,6 +177,11 @@ static void	do_sort_stacks(t_stacks *stacks, size_t n)
 	do_sort_stacks(stacks, n - pushed);
 	while (pushed > 0)
 	{
+		if (deque_size(stacks->b) <= 25)
+		{
+			f(stacks);
+			break;
+		}
 		if (pushed > 2)
 			restored = restore_half(stacks, pushed);
 		else
